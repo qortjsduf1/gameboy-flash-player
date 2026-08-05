8dd928bdb3c52ae414905efeb3718d3a82cb13f4
백선열 <baekseon-yeol@baegseon-yeol-ui-MacBookAir.local>
2026-08-05 17:11:12 +0900

    Use CommonJS export in api/proxy.js for Vercel Node compatibility

--- api/proxy.js
+++ api/proxy.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', '*');
@@ -37,4 +37,4 @@ export default async function handler(req, res) {
   } catch (err) {
     res.status(500).send(`Proxy Error: ${err.message}`);
   }
-}
+};
